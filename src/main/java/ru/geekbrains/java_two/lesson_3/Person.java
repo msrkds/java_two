ef2376d27defb1f45fd08f9c9006872725b19a1f
msrkds <kifel.ds@gmail.com>
2022-05-03 04:35:39 +0600

    HomeWork_3

---
+++ src/main/java/ru/geekbrains/java_two/lesson_3/Person.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.java_two.lesson_3;
+
+public class Person {
+    public String name;
+    public String phone;
+
+    public Person(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+}
